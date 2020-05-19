2217f2846008455232f4c33614e4bad3d22c53ce
khoi148 <khoiminhphan@yahoo.com>
2020-05-19 17:39:59 +0700

    backend features for multiple users and rooms

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.DB, {
 
 //syntax: use 'connection' string, then specify socket strings
 io.on("connection", async (socket) => {
-  socket.emit("rooms", await Room.find()); //simply return an array of rooms
+  socket.emit("rooms", await Room.find()); //simply return an array of rooms, when app starts
 
   socket.on("chat", (chatObj, cb) => {
     //console.log(chatObj); chatObj = {name:..., text:...}
@@ -38,6 +38,29 @@ io.on("connection", async (socket) => {
     } catch (err) {
       return cb({ ok: false, error: err.message });
     }
+  });
+  socket.on("joinRoom", async (roomId) => {
+    //1. Server class contains a User object, in this.user, and static methods for joining rooms
+    const server = await ServerClass.checkUser(socket.id);
+    console.log("server: ", server);
+    //2. Update database when user joins a room
+    await server.joinRoom(roomId);
+    //3. update the current room info to the client
+    socket.emit("selectedRoom", server.user.room);
+    //4 - subscribe to channel. Basically, Docs: https://socket.io/docs/rooms-and-namespaces/
+    socket.join(roomId);
+    //5 - send welcome message to client
+    socket.emit("chatlog", {
+      name: "System",
+      text: `Welcome ${server.user.name} to ${server.user.room.name}`,
+    });
+    //6. send notifications to other clients in the new room. Must use join() first to declare arbitrary 'rooms' in socketIO
+    socket.to(roomId).broadcast.emit("chatlog", {
+      name: "System",
+      text: `User ${server.user.name} has joined the server`,
+    });
+    //7 - update rooms globally to affect changes
+    io.emit("rooms", await Room.find());
   });
 });
 
--- utils/ServerClass.js
+++ utils/ServerClass.js
@@ -1,10 +1,14 @@
 const User = require("../models/user");
-class Server {
-  constructor() {}
+const Room = require("../models/room");
 
+class Server {
+  constructor(user) {
+    this.user = user;
+  }
   //const server = new Server(id, name);
   static async login(userName, socketId) {
     let user = await User.findOne({ name: userName.toLowerCase() });
+    //add token to the user object, one way or another
     if (!user) {
       user = await User.create({
         name: userName.toLowerCase(),
@@ -14,6 +18,22 @@ class Server {
     user.token = socketId;
     await user.save();
     return user;
+  }
+  static async checkUser(socketId) {
+    const user = await User.findOne({ token: socketId }); //registered/logged in user with the token from that socket
+    console.log("user is: ", user);
+    if (!user) throw new Error("User not found: ", socketId);
+    return new Server(user);
+  }
+  async joinRoom(roomId) {
+    const room = await Room.findById(roomId);
+    if (!room.members.includes(this.user._id)) {
+      room.members.push(this.user._id);
+    }
+    console.log("room data", room);
+    this.user.room = roomId; //save roomID to database
+    await this.user.save();
+    this.user.room = room; //however, in JS, we will use the JS object
   }
 }
 module.exports = Server;
